26342b22ba160dd4ee1b1a57e092a077897960c3
Deva9754 <deva.sahu2001@gmail.com>
2024-02-02 23:10:39 +0530

    " changes complete"

--- src/examples/FunctionsLoops.js
+++ src/examples/FunctionsLoops.js
@@ -1,3 +1,5 @@
+//Arrays
+
 const marks = [59, 54, 67, 23, 67, 65, 78];
 
 const marks1 = [59, 54, 67, 23, 67, 65, 78];
@@ -23,66 +25,59 @@ const new_arr = marks.reduce((h1, h2) => {
   return h1 + h2;
 });
 console.log(new_arr);
-////
 
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
-// // Online Javascript Editor for free
-// // Write, Edit and Run your Javascript code using JS Online Compiler
-
+//Loops
 const marks3 = [59, 54, 67, 23, 67, 65, 78];
 // // for( i=0;i<marks.length;i++)
 // // console.log(marks[i])
 // // delete marks[0]
 // // console.log(marks.length)
 
-// const marks=[59,54,67,23,67,65,78]
+const marks2 = [59, 54, 67, 23, 67, 65, 78];
 
-// const marks1=[59,54,67,23,67,65,78]
-// const new_array=marks.concat(marks1)
-// console.log(new_array.length)
-// let compare = (a,b)=>{
-//     return a-b
-// }
-// console.log(marks.sort(compare))
-// console.log(marks.slice(2,6))
-// let arr = Array.from(marks)
-// console.log("Array from"+arr)
+const marks11 = [59, 54, 67, 23, 67, 65, 78];
+const new_array1 = marks.concat(marks1);
+console.log(new_array.length);
+let compare1 = (a, b) => {
+  return a - b;
+};
+console.log(marks.sort(compare));
+console.log(marks.slice(2, 6));
+let arr1 = Array.from(marks);
+console.log("Array from" + arr);
 
-// const student = marks.map((stu)=>{
-//     console.log("map"+stu)
-// }
-// )
-// let details = marks.filter((fdata)=>{
-//   return fdata>50
-// })
-//     console.log("fdata"+details)
+map, filter, reduce;
 
-//     const new_arr = marks.reduce((h1,h2)=>{
-//         return h1+h2
-//     })
-//     console.log(new_arr);
+const student1 = marks.map((stu) => {
+  console.log("map" + stu);
+});
+let details1 = marks.filter((fdata) => {
+  return fdata > 50;
+});
+console.log("fdata" + details);
 
-// let a;
-// do{
-//  a = prompt("enter a number");
-//  a =Number.parseInt(a);
-// marks.push(a)
-// }
-// while(a!=0)
-// console.log(marks)
+const new_arr1 = marks.reduce((h1, h2) => {
+  return h1 + h2;
+});
+console.log(new_arr);
 
-// const detail = marks.filter((data)=>{
-//     return data%2==0
+let ab;
+do {
+  a = prompt("enter a number");
+  a = Number.parseInt(a);
+  marks.push(a);
+} while (a != 0);
+console.log(marks);
 
-// })
-// console.log(detail);
+const detail = marks.filter((data) => {
+  return data % 2 == 0;
+});
+console.log(detail);
 
-// const square = marks.map((data)=>{
-//     return data*data
-// })
-// console.log(square)
+const square = marks.map((data) => {
+  return data * data;
+});
+console.log(square);
 
 let a = prompt("enter ur age ");
 a = Number.parseInt(a);
